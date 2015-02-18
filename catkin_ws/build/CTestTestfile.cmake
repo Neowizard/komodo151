f38152ba96454fe584aee9c31768ccf9f3412b60
Michael Bar-Sinai <mich.barsinai@gmail.com>
2015-02-18 13:18:28 +0200

    Removed binary folders of the catkin_ws

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/sf_ubuntu_ros/robotics_lab/catkin_ws/src
-# Build directory: /media/sf_ubuntu_ros/robotics_lab/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/media/sf_ubuntu_ros/robotics_lab/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
