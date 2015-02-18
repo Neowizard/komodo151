7bfa21a441605d22bfab45c5e99cd0f9793c33d1
Maor <maorashkenazi@gmail.com>
2015-02-18 20:21:03 +0200

    Second update to the Walk node

--- catkin_ws/src/cupinator/scripts/Walk.py
+++ catkin_ws/src/cupinator/scripts/Walk.py
@@ -2,46 +2,103 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
-
+from nav_msgs.msg import Odometry
+from cupinator.msg import WalkCommand
+from cupinator.msg import WalkResult
 
 collisionAlert = False
+linearDistanceAlert = False
+angularDistanceAlert = False
+linearDistance = 0
+angularDistance = 0
+PI = 3.14
+angleSample = 180
+minDistance = 0.25
 
 def checkCollision(laserData):
     global collisionAlert
+    global angleSample
+    global minDistance
+
+    startPoint = (180*512)/(laserData.angle_max - laserData.angle_min)
+    endPoint = laserData.angle_max-startPoint-1
+
+    for i in range(startPoint,endPoint):
+        if laserData.ranges[i]<=minDistance:
+            collisionAlert = True
+            break
 
 
-    if not collisionAlert:
+def checkDistanceTraveled(odom):
+    global linearDistanceAlert
+    global angularDistanceAlert
+    global linearDistance
+    global angularDistance
+    # TODO: TRAVELED
+    linearTraveled = odom.getTotalLinearTraveled()
+    if linearTraveled >= linearDistance:
+        linearDistanceAlert = True
+
+    angularTraveled = odom.getTotalAngularTraveled()
+    if angularTraveled > angularDistance:
+        angularDistanceAlert = True
 
 
-    #print("[checkProxCB] Range: " + str(rangeData.range) + " proxAlert: " + str(collisionAlert))
 
+def walkUntilCollision(cmd):
+    global linearDistance
+    global angularDistance
+    global collisionAlert
+    global linearDistanceAlert
+    global angularDistanceAlert
+    global PI
 
-def scanChecker():
-    rospy.Subscriber('/komodo_1/scan', LaserScan, checkCollision)
-    
+    linearDistance = cmd.distance
 
-def walkUntilCollision():
     walkPub = rospy.Publisher('/komodo_1/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('Walk', anonymous=True)
-    rate = rospy.Rate(10) 
-    while not rospy.is_shutdown():
-	#print("[Publisher] proxAlert: " + str(collisionAlert))
-        walkTwist = Twist()
-        if (collisionAlert == False):
-            walkTwist.linear.x = 0.7
-        else:
-            walkTwist.linear.x = 0.0
 
+    rospy.Subscriber('/komodo_1/odom_pub', Odometry, checkDistanceTraveled)
+
+    rate = rospy.Rate(10)
+    walkTwist = Twist()
+    while not (angularDistanceAlert or collisionAlert):
+        walkTwist.angular.z = 0.3*PI
         walkPub.publish(walkTwist)
         rate.sleep()
 
-def waitForCommand():
-    rospy.Subscriber('/cupinator/walk/command',
+    walkTwist.angular.z = 0
+    walkPub.publish(walkTwist)
+
+    while not (linearDistanceAlert or collisionAlert):
+        walkTwist.linear = 0.5
+        walkPub.publish(walkTwist)
+        rate.sleep()
+
+
+    # PUBLISH 0 SPEED TO MAKE IT STOP MOVING
+    walkTwist.linear.x = 0.0
+    walkPub.publish(walkTwist)
+    # ------------------------
+
+    # RESTORE THE VALUES FOR THE GLOBAL VARIABLES
+    collisionAlert = False
+    linearDistanceAlert = False
+    angularDistanceAlert = False
+    linearDistance = 0
+    angularDistance = 0
+    # -----------------
+
+    # PUBLISH WALK RESULT
+    walkPub = rospy.Publisher('/cupinator/walk/result', WalkResult, queue_size=2)
+    res = WalkResult()
+    res.result = not collisionAlert # if collisionAlert is true, we failed to move all the way
+    walkPub.publish(res)
+    # ------------------
 
 if __name__ == '__main__':
     try:
-        waitForCommand()
-        scanChecker()
-        walkUntilCollision()
+        rospy.init_node('Walk', anonymous=True)
+        rospy.Subscriber('/komodo_1/scan', LaserScan, checkCollision)
+        rospy.Subscriber('/cupinator/walk/command', WalkCommand, walkUntilCollision)
     except rospy.ROSInterruptException:
         pass
