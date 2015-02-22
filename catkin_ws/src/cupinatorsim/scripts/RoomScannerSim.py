6733b45c9567497b35a0a6bd6d90304b0fe25b2a
Michael Bar-Sinai <mich.barsinai@gmail.com>
2015-02-22 19:18:12 +0200

    cleaner python code

--- catkin_ws/src/cupinatorsim/scripts/RoomScannerSim.py
+++ catkin_ws/src/cupinatorsim/scripts/RoomScannerSim.py
@@ -12,18 +12,15 @@ class RoomScannerSim:
         self.distance = initial_distance
         self.publisher = rospy.Publisher("/cupinator/room_scanner/result", RoomScannerResult, queue_size=5)
         self.first_scan = True
+        rospy.Subscriber("/cupinator/room_scanner/command", RoomScannerCommand, self.scan_room)
 
-    def scan_room(self):
+    def scan_room(self, cmd):
+        print "Got scan message: ", repr(cmd)
         cur_dist = self.distance
         self.distance /= 2
         cur_dist = 0 if cur_dist < 10 else cur_dist
-        rospy.sleep(2.)
+        rospy.sleep(1.)
         self.publisher.publish(RoomScannerResult(None, 10.0, 5, cur_dist))
-
-
-def callback(scan_msg):
-    print "Got scan message: ", repr(scan_msg)
-    walk_instance.scan_room()
 
 
 if __name__ == "__main__":
@@ -31,9 +28,7 @@ if __name__ == "__main__":
     print "Stating simulation of Room Scanner...",
 
     rospy.init_node("RoomScannerSim", anonymous=False)
-
     walk_instance = RoomScannerSim(1000)
-    rospy.Subscriber("/cupinator/room_scanner/command", RoomScannerCommand, callback)
 
     print "started."
 
--- catkin_ws/src/cupinatorsim/scripts/WalkerSim.py
+++ catkin_ws/src/cupinatorsim/scripts/WalkerSim.py
@@ -11,15 +11,12 @@ class WalkSim:
     def __init__(self):
         self.publisher = rospy.Publisher("/cupinator/walk/result", WalkResult, queue_size=5)
         self.first_scan = True
+        rospy.Subscriber("/cupinator/walk/command", WalkCommand, self.walk)
 
-    def walk(self):
+    def walk(self, walk_msg):
+        print "Got walk message: ", repr(walk_msg)
         rospy.sleep(2.)
         self.publisher.publish(WalkResult(None, True))
-
-
-def callback(walk_msg):
-    print "Got walk message: ", repr(walk_msg)
-    walk_instance.walk()
 
 
 if __name__ == "__main__":
@@ -27,9 +24,7 @@ if __name__ == "__main__":
     print "Stating simulation of Walk Module...",
 
     rospy.init_node("WalkSim", anonymous=False)
-
     walk_instance = WalkSim()
-    rospy.Subscriber("/cupinator/walk/command", WalkCommand, callback)
 
     print "started."
 
